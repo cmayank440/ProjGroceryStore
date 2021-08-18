ef08e1a7ab47caf43b94ea74c874bd18f8114809
Mayank Chaudhary <44719218+cmayank440@users.noreply.github.com>
2021-08-18 15:45:28 +0530

    Add files via upload

---
+++ Admin/AddItem.aspx
@@ -0,0 +1,85 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/MasterPage.Master" AutoEventWireup="true" CodeBehind="AddItem.aspx.cs" Inherits="ProjGroceryStore.Admin.AddItem" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <title>ADD ITEM </title>
+   
+     <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>ADD CATEGORY</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+     <div class="row">
+            <div class="col-sm-2"></div>
+            <div class="col-sm-8">
+                <div class="form-group">
+                    <label>    Item Name </label>
+                    <asp:TextBox ID="txtItemName" CssClass="form-control" runat="server"></asp:TextBox>
+                </div>
+
+                <div class="form-group">
+                    <label>    Description </label>
+                    <asp:TextBox ID="txtDesc" CssClass="form-control" runat="server"></asp:TextBox>
+                </div>
+
+                <div class="form-group">
+                    <label>    Price </label>
+                    <asp:TextBox ID="txtPrice" CssClass="form-control" runat="server"></asp:TextBox>
+                </div>
+
+                <div class="form-group">
+                    <label>    Quantity </label>
+                    <asp:TextBox ID="txtQty" CssClass="form-control" runat="server"></asp:TextBox>
+                </div>
+
+                <div class="form-group">
+                    <label>    Image </label>
+                    <asp:FileUpload ID="FileUpload1" CssClass="form-control" runat="server" />
+                </div>
+
+                <div class="form-group">
+                    <label>    Image1 </label>
+                    <asp:FileUpload ID="FileUpload2" CssClass="form-control" runat="server" />
+                </div>
+
+                <div class="form-group">
+                    <label>    Image2 </label>
+                    <asp:FileUpload ID="FileUpload3" CssClass="form-control" runat="server" />
+                </div>
+
+               <%-- <div class="form-group">
+                    <label>    Size </label>
+                    <asp:DropDownList ID="ddlSize" CssClass="form-control" runat="server"></asp:DropDownList>
+                </div>--%>
+
+                  <div class="form-group">
+                    <label>    Category </label>
+                      <asp:DropDownList ID="ddlCategory" CssClass="form-control" runat="server"></asp:DropDownList>
+                </div>
+
+                <div class="form-group">
+                    <asp:Button ID="btnAddItem" CssClass="btn btn-success btn-lg" runat="server" Text="ADD ITEM" OnClick="btnAddItem_Click" />
+                </div>
+
+            </div>
+
+
+            <div class="col-sm-2"></div>
+
+        </div>
+
+
+
+    <div class="row">
+            <div class="col-sm-12"></div>
+                <div class="table">
+
+                    <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+
+                    </div>
+        </div>
+    
+</asp:Content>
---
+++ Admin/AddItem.aspx.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.IO;
+using System.Text;
+
+namespace ProjGroceryStore.Admin
+{
+    public partial class AddItem : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Username"] != null)
+            {
+                if (IsPostBack)
+                {
+                    // BindingGridView();
+                    BindCategory();
+                }
+            }
+            else
+            {
+                Response.Redirect("Login.aspx");
+            }
+        }
+
+
+        private string GetConnectionString()
+        {
+            return System.Configuration.ConfigurationManager.ConnectionStrings["OnlineGroceryProjectDBConnectionString"].ConnectionString;
+        }
+
+
+
+        protected void btnAddItem_Click(object sender, EventArgs e)
+        {
+            if (IsFormValidate())
+            {
+                //Insert record
+
+                //Insert Image first
+                FileUpload1.SaveAs(Server.MapPath("~/img/") + FileUpload1.FileName);
+                FileUpload2.SaveAs(Server.MapPath("~/img/") + FileUpload2.FileName);
+                FileUpload3.SaveAs(Server.MapPath("~/img/") + FileUpload3.FileName);
+                SqlConnection con = new SqlConnection(GetConnectionString());
+                SqlCommand cmd = new SqlCommand("insert into ItemMst (IName,Detail,Price,Image,Qnt,AvailQnt,SQnt,CategoryName,EntryDate,Image1,Image2) values (@IName,@Detail,@Price,@Image,@Qnt,@AvailQnt,@SQnt,@CategoryName,@EntryDate,@Image1,@Image2) ", con);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+
+                }
+                //cmd.Parameters.AddWithValue(" ", );
+                //cmd.Parameters.AddWithValue(" ", );
+            }
+            else
+            {
+                Response.Write("<script>alert('Validation Error');</script>");
+            }
+        }
+
+        private bool IsFormValidate()
+        {
+            if (txtItemName.Text==string.Empty)
+            {
+
+                Response.Write("<script>alert('Item name is required');</script>");
+                txtItemName.Text = string.Empty;
+                txtItemName.Focus();
+                return false;
+            }
+
+            if (txtDesc.Text==string.Empty)
+            {
+                Response.Write("<script>alert('Description Details is required');</script>");
+                txtDesc.Text = string.Empty;
+                txtDesc.Focus();
+                return false;
+            }
+
+            if (txtPrice.Text==string.Empty)
+            {
+                Response.Write("<script>alert('Price details is required');</script>");
+                txtPrice.Text = string.Empty;
+                txtPrice.Focus();
+                return false;
+            }
+            if (txtQty.Text==string.Empty)
+            {
+                Response.Write("<script>alert('Quantity details is required');</script>");
+                txtQty.Text = string.Empty;
+                txtQty.Focus();
+                return false;
+            }
+            if (ddlCategory.SelectedItem.Text==" ")
+            {
+                Response.Write("<script>alert('Category is required');</script>");
+                
+                ddlCategory.Focus();
+                return false;
+            }
+
+
+            return true;
+        }
+
+        private void BindCategory()
+        {
+            SqlConnection con = new SqlConnection(GetConnectionString());
+            SqlCommand cmd = new SqlCommand("",con);
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                ddlCategory.DataSource = dt;
+                ddlCategory.DataTextField = "CName";
+                ddlCategory.DataValueField = "CID";
+                ddlCategory.DataBind();
+            }
+            else
+            {
+                ddlCategory.DataSource = null;
+                ddlCategory.DataBind();
+
+            }
+        }
+
+
+
+
+
+
+
+
+    }
+}
